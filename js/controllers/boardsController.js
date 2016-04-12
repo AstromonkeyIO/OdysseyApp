a29179b295b0f5498f2960e3d328fb9637bc6575
Tom <teryun93@gmail.com>
2016-04-12 02:26:42 -0700

    ui redesigned

--- js/controllers/boardsController.js
+++ js/controllers/boardsController.js
@@ -141,4 +141,26 @@ odysseyApp.controller('boardsController', function($rootScope, $scope, $routePar
 		$scope.boardPopupState = "create";			
 	}
 
+
+    //code for left menu bar
+    $(function () {
+        /* START OF DEMO JS - NOT NEEDED */
+        if (window.location == window.parent.location) {
+            $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
+            $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+            $('#fullscreen').attr('title', 'Back To Bootsnipp');
+        }    
+        $('#fullscreen').on('click', function(event) {
+            event.preventDefault();
+            window.parent.location =  $('#fullscreen').attr('href');
+        });
+        $('#fullscreen').tooltip();
+        $('.navbar').hover(function(event) {
+            event.preventDefault();
+            $(this).closest('.navbar-minimal').toggleClass('open');
+        })
+    });
+
+	
+
 });
--- js/controllers/workflowController.js
+++ js/controllers/workflowController.js
@@ -370,12 +370,26 @@ odysseyApp.controller('workflowController', function ($scope, $routeParams, $com
 
     }
 
-    $(document).keyup(function (e) {
-        if (e.which == 27 && $('body').hasClass('modal-open')) {
-            console.log('esc');
 
-        }
+    //code for left menu bar
+    $(function () {
+        /* START OF DEMO JS - NOT NEEDED */
+        if (window.location == window.parent.location) {
+            $('#fullscreen').html('<span class="glyphicon glyphicon-resize-small"></span>');
+            $('#fullscreen').attr('href', 'http://bootsnipp.com/mouse0270/snippets/PbDb5');
+            $('#fullscreen').attr('title', 'Back To Bootsnipp');
+        }    
+        $('#fullscreen').on('click', function(event) {
+            event.preventDefault();
+            window.parent.location =  $('#fullscreen').attr('href');
+        });
+        $('#fullscreen').tooltip();
+        $('.navbar').hover(function(event) {
+            event.preventDefault();
+            $(this).closest('.navbar-minimal').toggleClass('open');
+        })
     });
+
 
 
     function displayTaskDetailPopup(taskName)
